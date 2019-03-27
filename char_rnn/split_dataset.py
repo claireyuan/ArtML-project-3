2394f8fe82be481e562793e20b34efc2df171adb
Claire Yuan <claireyuan97@gmail.com>
2019-03-26 21:40:57 -0400

    add script to split train/valid/test

---
+++ char_rnn/split_dataset.py
@@ -0,0 +1,22 @@
+import csv
+import sys
+import random
+
+test_percent = 10
+valid_percent = 10
+
+
+with open(train_file, 'w') as train, open(valid_file, 'w') as valid, open(test_file, 'w') as test:
+	for filename in sys.argv[1:]:
+		with open(filename, 'r') as csvfile:
+			reader = csv.reader(csvfile)
+			for row in reader:
+				# only keep the tweets not the metadata
+				tweet = row[2]
+				rand = random.randint(0, 100)
+				if (rand < test_percent):
+					test.write(tweet)
+				elif (rand < test_percent + valid_percent):
+					valid.write(tweet)
+				else:
+					train.write(tweet)
