80472d2686f29f299e04d5ebbe057ec40f90a744
Claire Yuan <claireyuan97@gmail.com>
2019-03-26 22:04:59 -0400

    training script

---
+++ char_rnn/train_generate.sh
@@ -0,0 +1,18 @@
+python main.py --cuda \
+	--data data \
+	--model GRU \
+	--emsize 32 \
+	--nhid 512 \
+	--nlayers 2 \
+	--batch_size 32 \
+	--bptt 141 \
+	--lr 20 \
+	--clip 0.25 \
+	--epochs 30 \
+	--save model${1}.pt 2>&1 | tee -a train${1}.log
+
+python generate.py --cuda \
+	--data data \
+	--checkpoint ./model${1}.pt \
+	--outf generated${1}.txt \
+	--words 1000 2>&1 | tee -a train${1}.log
