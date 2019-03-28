dade6071291ed5db7a03e326d36d516dd64c13e4
Claire Yuan <claireyuan97@gmail.com>
2019-03-28 10:59:08 -0400

    tune hyperparameters

--- char_rnn/train_generate.sh
+++ char_rnn/train_generate.sh
@@ -4,13 +4,13 @@ python main.py --cuda \
 	--data data \
 	--model GRU \
 	--emsize 32 \
-	--nhid 512 \
+	--nhid 256 \
 	--nlayers 2 \
-	--batch_size 32 \
+	--batch_size 64 \
 	--bptt 141 \
 	--lr 20 \
 	--clip 0.25 \
-	--epochs 30 \
+	--epochs 100 \
 	--save model${1}.pt 2>&1 | tee -a train${1}.log
 
 python generate.py --cuda \
